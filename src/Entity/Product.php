3ad0f2fc1637edd7a6441ae091c052cd5e134303
johangaub <johangaub@gmail.com>
2021-05-19 18:13:16 +0200

    Product.php: add Assert on url property

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -7,6 +7,7 @@ use App\Repository\ProductRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource()
@@ -33,6 +34,10 @@ class Product
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Url(
+     *    message = "The url '{{ value }}' is not a valid url",
+     *    protocols = {"http", "https", "ftp"}
+     * )
      */
     private $url;
 
